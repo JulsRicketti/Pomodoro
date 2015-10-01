efd30dfac7cd559a4ec5cfc56086d81736291ff6
Julia Ricketti <julsricketti@gmail.com>
2015-10-01 16:57:16 -0400

    fully working basic timer function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
-function countDown(secs, elem){
+function countDown(seconds, elem){
 	var element = document.getElementById(elem);
-	element.innerHTML = "Please wait for "+secs + " seconds";
-	
-	secs--;
-	
-	if(secs<0){
-		clearTimeout(timer);
-		element.innerHTML = '<h2>Complete!</h2>';
+	var minutes=0;
+	var totalSeconds=0;
+	if(seconds<=60){
+
+		element.innerHTML = "Please wait for "+seconds + " seconds";
 	}
-	var timer = setTimeout ('countDown('+secs+', "'+elem+'")', 1000);
-}
+	else{
+		var minutes = Math.floor(seconds/60);
+		seconds= seconds -(minutes*60);
+		
+		element.innerHTML = "Please wait for "+minutes+ " minutes and "+seconds+ " seconds";
+	}
+	
+	seconds--;
+	
+	if(seconds==0){
+		
+		if(minutes==0){
+			clearTimeout(timer);
+			element.innerHTML = '<h2>Complete!</h2>';
+		}
+		else{
+			minutes--;
+			seconds=59;
+			totalSeconds = seconds + (minutes*60);
+			var timer = setTimeout ('countDown('+totalSeconds+', "'+elem+'")', 1000);
+		}
+	}
+	else{
+		if(seconds==0)
+			seconds=59;
+		totalSeconds = seconds + (minutes*60);
+		var timer = setTimeout ('countDown('+totalSeconds+', "'+elem+'")', 1000);
+	}
+
+}	
+
