3c5b3e209f407e598b9b27c9d9bd5baeccd5ac80
Julia Ricketti <julsricketti@gmail.com>
2015-11-05 18:44:37 -0500

    Switched most scripts to the script.js file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
+var workTime = 25;
+var restTime = 15;
+var repeats =4;
+var longRestTime =30;
+function setCustomTime(setWorkTime){
+
+    workTime = setWorkTime;
+    alert(workTime);
+}
+
+function setCutomRestTime(setRestTime){
+    restTime = setRestTime;
+}
+
+function setCustomRepeat(setRepeatTime){
+    repeats = setRepeatTime;
+}
+
+function traditionalPomodoroStart(){
+    $(".new-form").hide();
+    workOrRestMsg(true, "status");
+    countDown(1500, "traditional");
+}
+
+function showNewForm(){
+    $(".new-form").show();
+}
+
+function startCustomPomodoro(){
+    $(".new-form").hide();
+    workTime = document.getElementById('customTime').value;
+    restTime=document.getElementById('customRestTime').value;
+    repeats=document.getElementById('customRepeat').value;
+    longRestTime =document.getElementById('customLongRestTime').value;
+    workOrRestMsg(true, "status");
+    countDown(workTime*60, "traditional");
+}
+
 function workOrRestMsg(isWorkTime, elem){
 	var element = document.getElementById(elem);
     if(isWorkTime)
